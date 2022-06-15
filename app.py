b7ca036231f9799914caa8f63d120e7c631c97a2
tdhood <taylordannhood@gmail.com>
2022-06-15 12:30:48 -0700

    templates and route added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,11 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.get('/')
+def show_users():
+    """Shows the list of existing users"""
+
+    users = User.query.all()
+    return render_template('user_listing.html', users=users)
